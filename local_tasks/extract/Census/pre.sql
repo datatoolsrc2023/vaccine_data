14f558493ee38cd4e3b0b608f9e53e12fbd8d879
blakedaniel <blake.vanfleteren@gmail.com>
2023-05-30 16:36:51 -0400

    moved previous worker files to utilities

--- local_tasks/extract/Census/pre.sql
+++
@@ -1,5 +0,0 @@
-SELECT "ESTIAP", year, count(*) as count
-FROM vaccines
-WHERE "ESTIAP" IN ('IL-CITY OF CHICAGO', 'NY-CITY OF NEW YORK')
-GROUP BY "ESTIAP", year
-ORDER BY "ESTIAP", year;
--- local_tasks/extract/NIH/common.R
+++
@@ -1,4 +0,0 @@
-
-# common paths used
-PUF <- "data/NIH_child/NIH_conv"
-flatfile_path <- "data/NIH_child/NIH_raw"
